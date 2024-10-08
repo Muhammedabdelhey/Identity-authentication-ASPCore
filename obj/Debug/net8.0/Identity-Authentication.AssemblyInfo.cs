1422418d30da8466f0a8f74d636666018f358cec
Muhammedabdelhey <muhammedabdelhay1@gmail.com>
2024-10-08 12:12:30 +0300

    add git ignore

--- obj/Debug/net8.0/Identity-Authentication.AssemblyInfo.cs
+++ obj/Debug/net8.0/Identity-Authentication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Identity-Authentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b6877ed999719c958e9839f857a78b8e3755ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+894eae399083c4f5eeebe4e6636a8b4775b431bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Identity-Authentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Identity-Authentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
